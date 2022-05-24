dab90ba5979015c2717095def063cf5bdd4b5267
KUD <kimud6003@gmail.com>
2022-05-25 04:00:05 +0900

    axios

--- web/src/Components/Input/index.jsx
+++ web/src/Components/Input/index.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable */ 
 import {React, useState, useEffect, useRef} from 'react';
+import axios from 'axios';
 import InputStyle from './style';
 
 function Input() {
@@ -6,6 +8,7 @@ function Input() {
   const [previewURL, setPreviewURL] = useState('');
   const [preview, setPreview] = useState(null);
   const imageRef = useRef();
+  const URL = 'http://localhost:8000/sticker/'
 
   useEffect(() => {
     if (image !== '')
@@ -19,14 +22,26 @@ function Input() {
     e.preventDefault();
     const getImage = e.target.files[0];
     const reader = new FileReader();
+    let form_data = new FormData();
+      console.log(image)
+
+      form_data.append('image', getImage);
+
+      let url = 'http://localhost:8000/sticker/';
+
+      axios.post(url, form_data, {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      }).then(res => {
+          console.log(res.data);
+        }).catch(err => console.log(err)) 
 
     reader.onloadend = () => {
-      setImage(getImage);
-      setPreviewURL(reader.result);
+      setImage(() => getImage);
     };
-    if (getImage) reader.readAsDataURL(getImage);
-  };
 
+  };
   const imageOnClick = e => {
     e.preventDefault();
     imageRef.current.click();
