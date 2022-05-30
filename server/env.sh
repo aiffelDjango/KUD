e28b2e187e4ee621086c7fba7e612034d59f9168
kimud6003 <kimud6003@gmail.com>
2022-05-30 14:34:51 +0000

    env update

--- server/env.sh
+++ server/env.sh
@@ -23,3 +23,15 @@ bash ~/.bashprofile
 pyenv install 3.8.7
 
 pyenv global 3.8.7
+
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+
+cat /etc/apt/sources.list.d/nodesource.list
+
+deb https://deb.nodesource.com/node_16.x focal main
+
+deb-src https://deb.nodesource.com/node_16.x focal main
+
+sudo apt -y install nodejs
+
+sudo apt -y install npm
